a5d6c9dc16cc67d88e12aff60cbd4d9c0b583854
Yahir2005 <luisyajairo@outlook.com>
2026-07-20 20:05:46 -0600

    feat(usuario): integrar UsuarioService y señales para cargar usuarios desde JSON

--- Frontend/src/app/features/usuario/pages/lista-usuario/lista-usuario.ts
+++ Frontend/src/app/features/usuario/pages/lista-usuario/lista-usuario.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UsuarioService } from '../../services/usuario';
+import { Usuario } from '../../models/usuario.model';
 
 @Component({
   selector: 'app-lista-usuario',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './lista-usuario.html',
   styleUrl: './lista-usuario.scss',
 })
-export class ListaUsuario {}
+export class ListaUsuario {
+
+  private usuarioService = inject(UsuarioService);
+  usuario = signal<Usuario[]>([]);
+  loading = signal(false);
+  error = signal<string | null>(null);
+  ngOninit(): void {
+    this.loading.set(true);
+    this.usuarioService.getUsuarios().subscribe({
+      next: (data) => {
+        this.usuario.set(data);
+        this.loading.set(false);
+      },
+      error: () => {
+        this.error.set('Error al cargar los usuarios');
+        this.loading.set(false);
+      }
+    });
+  }
+}
