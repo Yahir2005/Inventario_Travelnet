8813fda4f807ee5a5c3afae9b8554fa71b6730c6
Yahir2005 <luisyajairo@outlook.com>
2026-07-20 22:46:09 -0600

    feat(cliente): agregar componente cliente

---
+++ Frontend/src/app/features/cliente/pages/lista-cliente/lista-cliente.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-cliente',
+  imports: [],
+  templateUrl: './lista-cliente.html',
+  styleUrl: './lista-cliente.scss',
+})
+export class ListaCliente {}
