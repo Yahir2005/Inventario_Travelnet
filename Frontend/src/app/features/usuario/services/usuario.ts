eb4d7a5f6bf03065ef30bac4130e61f8a5fd360d
Yahir2005 <luisyajairo@outlook.com>
2026-07-20 20:01:39 -0600

    feat(usuario): agregar HttpClient getUsuario para consumir JSON

--- Frontend/src/app/features/usuario/services/usuario.ts
+++ Frontend/src/app/features/usuario/services/usuario.ts
@@ -1,6 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Usuario } from '../models/usuario.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Usuario {}
+export class UsuarioService {
+  constructor(private http: HttpClient) { }
+  private readonly dataUrl = '/public/assets/data/empleados.json';
+  getUsuarios(): Observable<Usuario[]> {
+    //return this.http.get<Usuario[]>('/public/assets/data/empleados.json');
+    return this.http.get<Usuario[]>(this.dataUrl);
+  }
+}
