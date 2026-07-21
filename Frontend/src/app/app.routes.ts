524869af01359aa9e879aaf18bba38dab402aa18
Yahir2005 <luisyajairo@outlook.com>
2026-07-20 18:36:58 -0600

    feat(usuario):ts agregar ruta para usuario.routes en app.routes

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'usuario',
+        loadChildren: () =>
+            import('./features/usuario/usuario.routes').then(
+                (m) => m.USUARIO
+            )
+    }
+];
