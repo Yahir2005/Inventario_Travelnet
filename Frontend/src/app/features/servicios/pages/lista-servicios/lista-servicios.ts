457d5d86b51e0d7a663ddaa0f14aa0f21be50b6a
Yahir2005 <luisyajairo@outlook.com>
2026-07-22 21:11:12 -0600

    feat(servicios): agregar componentes servicios

---
+++ Frontend/src/app/features/servicios/pages/lista-servicios/lista-servicios.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-servicios',
+  imports: [],
+  templateUrl: './lista-servicios.html',
+  styleUrl: './lista-servicios.scss',
+})
+export class ListaServicios {}
