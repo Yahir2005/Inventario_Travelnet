1ba0750c1ded4bd71a4c71679a4776838991cd60
Yahir2005 <luisyajairo@outlook.com>
2026-07-23 00:00:51 -0600

    test(cliente): se agrego la dirección del json de clientes en clientes services

--- Frontend/src/app/features/cliente/services/cliente.ts
+++ Frontend/src/app/features/cliente/services/cliente.ts
@@ -1,6 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Cliente } from '../models/cliente.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Cliente {}
+export class ClienteService {
+  constructor(private http: HttpClient) {}
+  private readonly dataUrl = '/assets/data/clientes.json';
+  getClientes(): Observable<Cliente[]>{
+    return this.http.get<Cliente[]>(this.dataUrl);
+  }
+}
