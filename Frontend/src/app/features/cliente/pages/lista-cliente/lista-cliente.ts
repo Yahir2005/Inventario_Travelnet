3b64915a324dac54bd8f6d01b292d906aceb1872
Yahir2005 <luisyajairo@outlook.com>
2026-07-23 00:27:22 -0600

    feat(cliente): agregar consumo de clientes.json y señales en ListaCliente

--- Frontend/src/app/features/cliente/pages/lista-cliente/lista-cliente.ts
+++ Frontend/src/app/features/cliente/pages/lista-cliente/lista-cliente.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ClienteService } from '../../services/cliente';
+import { Cliente } from '../../models/cliente.model'
 
 @Component({
   selector: 'app-lista-cliente',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './lista-cliente.html',
   styleUrl: './lista-cliente.scss',
 })
-export class ListaCliente {}
+export class ListaCliente implements OnInit {
+  private clienteService = inject(ClienteService);
+
+  clientes = signal<Cliente[]>([]);
+  clienteSeleccionado = signal<Cliente | null>(null);
+  loading = signal(false);
+  
+  ngOnInit(): void {
+    this.loading.set(true);
+    this.clienteService.getClientes().subscribe({
+      next: (data) => {
+        this.clientes.set(data);
+        this.loading.set(false);
+      },
+      error: (err) => {
+        console.error('Error al cargar clientes',err);
+        this.loading.set(false);
+      }
+    });
+  }
+  verDetalles(cliente: Cliente) {
+    this.clienteSeleccionado.set(cliente);
+  }
+}
+
