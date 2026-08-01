f5496b3e784e04c9b871859f8cb7d3a15a8e9820
Yahir2005 <luisyajairo@outlook.com>
2026-08-01 14:05:13 -0600

    feat(server): agregar rutas de cliente al archivo app.js

--- Backend/app.js
+++ Backend/app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var getDbPool = require('./src/config/database');
 var usuarioRouter = require('./routes/Usuario');
+var clienteRouter = require('./routes/Cliente');
 
 var app = express();
 
@@ -30,6 +31,7 @@ app.use(function(req,res,next){
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/usuarios',usuarioRouter);
+app.use('/api/cliente',clienteRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
