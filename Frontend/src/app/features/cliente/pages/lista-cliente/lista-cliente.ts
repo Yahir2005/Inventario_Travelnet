600f09a80b36dd83afe7536557100528653dcc4e
Yahir2005 <luisyajairo@outlook.com>
2026-08-01 21:38:22 -0600

    fix(cliente): correciones listar clientes en pages

--- Frontend/src/app/features/cliente/pages/lista-cliente/lista-cliente.ts
+++ Frontend/src/app/features/cliente/pages/lista-cliente/lista-cliente.ts
@@ -1,36 +1,42 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ClienteService } from '../../services/cliente';
-import { ClienteListView } from '../../models/cliente-list-view.model';
+import { Component, inject, Inject, OnInit,signal,Signal } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { ClienteService } from "../../services/cliente";
+import { ClienteDetallado } from "../../models/cliente-list-view.model";
 
 @Component({
   selector: 'app-lista-cliente',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './lista-cliente.html',
-  styleUrl: './lista-cliente.scss',
+  styleUrls: ['./lista-cliente.scss']
 })
-export class ListaCliente implements OnInit {
-  clientes = signal<ClienteListView[]>([]);
+
+export class ListaClienteComponent implements OnInit {
+  private clienteService = inject(ClienteService);
+
+  clientes = signal<ClienteDetallado[]>([]);
   loading = signal<boolean>(true);
 
-  constructor(private clienteService: ClienteService) {}
-
   ngOnInit(): void {
-    this.clienteService.getClientesListView().subscribe({
+    this.cargarClientes();
+}
+  cargarClientes(){
+    this.loading.set(true);
+
+    this.clienteService.obtenerListaDetallada().subscribe({
       next: (data) => {
         this.clientes.set(data);
         this.loading.set(false);
       },
       error: (err) => {
-        console.error('Error al cargar clientes', err);
+        console.error('Error al cargar la lista de clientes',err);
         this.loading.set(false);
-      },
+      }
     });
   }
 
-  verDetalles(cliente: ClienteListView): void {
-    console.log(cliente);
-    // TODO: navegar a detalles o abrir modal
+  verDetalles(cliente:ClienteDetallado){
+    console.log('Motrando detalles del cliente: ',cliente.Nombre_Cliente);
   }
 }
