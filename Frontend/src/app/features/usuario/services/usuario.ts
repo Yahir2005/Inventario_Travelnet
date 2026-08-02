43af8ee2486c1f3c80c623910ba55f2f055bb556
Yahir2005 <luisyajairo@outlook.com>
2026-08-01 21:14:06 -0600

    feat(usuario): conectar servicio UsuarioService con API Express

--- Frontend/src/app/features/usuario/services/usuario.ts
+++ Frontend/src/app/features/usuario/services/usuario.ts
@@ -1,16 +1,17 @@
-import { inject, Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { tap } from 'rxjs/operators';
+import { inject,Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable,tap } from "rxjs";
+import { Usuario } from "../models/usuario.model";
 
 @Injectable({
   providedIn: 'root',
 })
-export class UsuarioService {
+export class UsuarioService{
   private http = inject(HttpClient);
-  private apiUrl = 'http://localhost:3000/api/usuarios/login';
+  private apiUrl = 'http://localhost:3000/api/usuarios'; 
 
   login(credenciales: {Usuario: string, Password: string}){
-    return this.http.post<any>(this.apiUrl, credenciales).pipe(
+    return this.http.post<any>(`${this.apiUrl}/login`,credenciales).pipe(
       tap(respuesta => {
         if(respuesta && respuesta.token){
           localStorage.setItem('token',respuesta.token);
@@ -20,7 +21,11 @@ export class UsuarioService {
     )
   }
 
-  estaAutenticado(): boolean{
+  estaAutenticado(): boolean {
     return !!localStorage.getItem('token');
+  }
+
+  getUsuarios():Observable <Usuario[]>{
+    return this.http.get<Usuario[]>(this.apiUrl);
   }
 }
