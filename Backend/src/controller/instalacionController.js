7790e60884cfb1d55e8f9f71cea5b07e8900e35b
Yahir2005 <luisyajairo@outlook.com>
2026-08-02 22:41:52 -0600

    feat(server): agregar controlador instalación

---
+++ Backend/src/controller/instalacionController.js
@@ -0,0 +1,54 @@
+const Instalacion = require('../model/instalacion');
+
+const getAll = async (req, res) => {
+    try {
+        const instalacion = await Instalacion.findAll(req.db);
+        res.json(instalacion);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const getById = async (req, res) => {
+    try {
+        const instalacion = await Instalacion.findByPk(req.db, req.params.id);
+        if(!instalacion) return res.status(404).json({ error: 'Instalación no encontrada' });
+        res.json(instalacion);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const create = async (req, res) => {
+    try {
+        const instalacion = await Instalacion.create(req.db, req.body);
+        res.status(201).json(instalacion);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const update = async (req, res) => {
+    try {
+        const exist = await Instalacion.findByPk(req.db, req.params.id);
+        if(!exist) return res.status(404).json({ error: 'Instalacion no encontrada' });
+
+        // Se agregó req.db
+        const instalacionUpdated = await Instalacion.update(req.db, req.params.id, req.body);
+        res.json(instalacionUpdated);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const remove = async (req, res) => {
+    try {
+        const deleted = await Instalacion.remove(req.db, req.params.id);
+        if(!deleted) return res.status(404).json({ error: 'Instalacion no encontrada' });
+        res.json({ message: 'Instalacion desactivada correctamente' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+module.exports = { getAll, getById, create, update, remove };
