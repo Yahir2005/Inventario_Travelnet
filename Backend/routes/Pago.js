f6fd60c8f7f7dc0e3dded643b6acaf6454cfaaf4
Yahir2005 <luisyajairo@outlook.com>
2026-08-04 17:17:35 -0600

    feat(server): agregar enrutador Pago

---
+++ Backend/routes/Pago.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var pagoController = require('../src/controller/pagoController');
+
+router.get('/',pagoController.getAll);
+router.get('/:id',pagoController.getById);
+router.post('/',pagoController.create);
+router.put('/:id',pagoController.update);
+
+module.exports = router;
