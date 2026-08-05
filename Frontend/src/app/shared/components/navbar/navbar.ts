80afae2ed1ee93570e9094b6dd334693d82e9ad9
Yahir2005 <luisyajairo@outlook.com>
2026-08-04 18:53:43 -0600

    feat(shared): agregar control de roles en navbar para restringir acceso según permisos

--- Frontend/src/app/shared/components/navbar/navbar.ts
+++ Frontend/src/app/shared/components/navbar/navbar.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router,RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule],
+  imports: [RouterModule,CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
-export class Navbar {}
+export class Navbar implements OnInit{
+  private router = inject(Router);
+  public ocupacionUsuario: string = 'Cargando ...';
+
+  public esAdmin: boolean = false;
+
+  ngOnInit(): void {
+    this.obtenerOcupacion();
+  }
+
+  obtenerOcupacion(): void {
+    const usuarioString = localStorage.getItem('usuario');
+
+    if(usuarioString){
+      const usuario = JSON.parse(usuarioString);
+      this.ocupacionUsuario = usuario.Ocupacion;
+
+      this.esAdmin = (usuario.Ocupacion === 'Administrador');
+    }else {
+      this.ocupacionUsuario = 'Desconocido';
+      this.esAdmin = false;
+    }
+  }
+
+  cerrarSesion(): void {
+    localStorage.removeItem('token');
+    localStorage.removeItem('usuario');
+
+    this.router.navigate(['/login']);
+  }
+}
