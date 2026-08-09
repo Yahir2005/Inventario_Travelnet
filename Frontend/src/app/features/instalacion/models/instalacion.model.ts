4f2e865a1544e0541ae3baa0520a854674fb4e70
Yahir2005 <luisyajairo@outlook.com>
2026-08-08 23:10:39 -0600

    feat(instalacion): se agregaron los campos Plan, Modalidad_Servicio a instalacion.model.ts

--- Frontend/src/app/features/instalacion/models/instalacion.model.ts
+++ Frontend/src/app/features/instalacion/models/instalacion.model.ts
@@ -9,5 +9,7 @@ export interface Instalacion{
     Password_Wifi: string;
     Active: boolean;
     Tipo: 'Fibra' | 'Antena';
+    Plan?: '20 MEGAS' | '40 MEGAS' | '60 MEGAS' | '80 MEGAS' | '100 MEGAS';
+    Modalidad_Servicio?: 'Mensual' | 'Bimestral' | 'Trimestral' | 'Cuatrimestral' | 'Quinquemestral' | 'Semestral' | 'Heptamestral' | 'Octomestral' | 'Nonamestral' | 'Decamestral' | 'Oncemestral' | 'Anual';
     Fecha_Instalacion: string;
 }
