d2b4078bbf633664a3f2c4b57251334ba46285b6
Yahir2005 <luisyajairo@outlook.com>
2026-08-08 23:05:31 -0600

    feat!(cliente): se actualizo la estructura de pago.model.ts

--- Frontend/src/app/features/cliente/models/pago.model.ts
+++ Frontend/src/app/features/cliente/models/pago.model.ts
@@ -2,15 +2,12 @@ export interface Pago {
     PagoId: number;
     InstalacionId: number;
     UsuarioId: number;
-    Modalidad_Servicio: 'Mensual' | 'Bimestral' | 'Trimestral' | 'Anual' | 'Otro';
-    Otra_Modalidad: string;
     Fecha_Pago: string;
-    Tipo_Pago: 'Efectivo' | 'Transferencia' | 'Otro';
-    Otro_Pago: string;
+    Tipo_Pago: 'Efectivo' | 'Transferencia' | 'Cheque' | 'Trueque' | 'Paypal' | 'MercadoPago' | 'Pagaré';
     Numero_cuenta: string;
+    Descuento: number | null;
     Estado_Pago: 'Completado' | 'Incompleto' | 'Pendiente';
     Monto: number;
-    Plan: '20 MEGAS' | '40 MEGAS' | '60 MEGAS' | '80 MEGAS' | '100 MEGAS';
     Uuid_local: string;
     Sincronizado: string;
     Ultima_modificacion: string;
