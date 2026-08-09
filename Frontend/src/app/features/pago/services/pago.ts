57b3ef01d17626394f6e70b2643ab64818d2b616
Yahir2005 <luisyajairo@outlook.com>
2026-08-08 23:23:50 -0600

    feat(pago): se agrego el endPoint de pago al servicio pago.ts

--- Frontend/src/app/features/pago/services/pago.ts
+++ Frontend/src/app/features/pago/services/pago.ts
@@ -1,6 +1,28 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Pago, PagoDetallado, PagoForm } from '../models/pago.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Pago {}
+export class PagoService {
+  private http = inject(HttpClient);
+  private apiUrl = 'http://localhost:3000/api/pago';
+
+  getPagos(): Observable<PagoDetallado[]> {
+    return this.http.get<PagoDetallado[]>(this.apiUrl);
+  }
+
+  getPago(id: number): Observable<Pago> {
+    return this.http.get<Pago>(`${this.apiUrl}/${id}`);
+  }
+
+  crearPago(datos: PagoForm): Observable<Pago> {
+    return this.http.post<Pago>(this.apiUrl, datos);
+  }
+
+  actualizarPago(id: number, datos: Partial<PagoForm>): Observable<Pago> {
+    return this.http.put<Pago>(`${this.apiUrl}/${id}`, datos);
+  }
+}
