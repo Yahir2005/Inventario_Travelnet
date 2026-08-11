b8be3081c07d892db972335555101523a8f3975d
Yahir2005 <luisyajairo@outlook.com>
2026-08-11 15:28:29 -0600

    feat(server): se agrego al enrutador Mensualidad las rutas de los metodos getByInstalación y cancelar

--- Backend/routes/Mensualidad.js
+++ Backend/routes/Mensualidad.js
@@ -3,7 +3,9 @@ var router = express.Router();
 var mensualidadController = require('../src/controller/mensualidadController');
 
 router.get('/', mensualidadController.getAll);
+router.get('/instalacion/:instalacionId', mensualidadController.getByInstalacion);
 router.get('/:id', mensualidadController.getById);
+router.post('/cancelar', mensualidadController.cancelar);
 router.post('/', mensualidadController.create);
 router.put('/:id', mensualidadController.update);
 router.delete('/:id', mensualidadController.remove);
