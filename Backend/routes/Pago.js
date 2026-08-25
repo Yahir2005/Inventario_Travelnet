d485cc7933880c937c319e1a0f42dcfdf1c16a9e
Yahir2005 <luisyajairo@outlook.com>
2026-08-24 23:27:52 -0600

    feat(server): se agrego la ruta del metodo getDesglose en el enrutador pago

--- Backend/routes/Pago.js
+++ Backend/routes/Pago.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var pagoController = require('../src/controller/pagoController');
 
 router.get('/',pagoController.getAll);
+router.post('/desglose', pagoController.getDesglose);
 router.get('/:id',pagoController.getById);
 router.post('/',pagoController.create);
 router.put('/:id',pagoController.update);
